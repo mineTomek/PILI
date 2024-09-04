fd5b6234c124967811448773cd956612cc06e764
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-04 15:02:46 +0200

    Move the component directory back

--- components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="h-12 flex w-full justify-center justify-content-center">
-        <h1 className="font-black text-3xl my-auto tracking-wider">PILI</h1>
-    </div>
-  );
-}
