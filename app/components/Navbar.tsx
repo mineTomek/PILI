284b115923538b0618ee2aaf7324f1092266d9f9
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-11 14:23:04 +0200

    Re-add the Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="h-12 fixed top-0 left-0 right-0">
+    <div className="flex justify-center">
+        <h1 className="font-black text-3xl my-auto tracking-wider">PILI</h1>
+    </div>
+    </div>
+  );
+}
