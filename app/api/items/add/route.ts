7d328c3a46d3e32886f25c247968e14b93b3804a
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-13 14:58:01 +0200

    Add Author ID logging with uuid

--- app/api/items/add/route.ts
+++ app/api/items/add/route.ts
@@ -1,11 +1,35 @@
 import { v4 as uuid } from "uuid";
 import fs from "fs";
 import path from "path";
-import { NextResponse } from "next/server";
 import Item from "@/utils/structures/Item";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const newItem: Item = await request.json();
+export async function POST(request: NextRequest) {
+  let newItem: Item;
+
+  try {
+    newItem = await request.json();
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Invalid JSON body", error: error.message },
+      { status: 400 }
+    );
+  }
+
+  if (!newItem) {
+    return NextResponse.json(
+      { message: "Request body cannot be empty" },
+      { status: 400 }
+    );
+  }
+
+  let deviceToken = request.cookies.get("device-token")?.value;
+
+  if (!deviceToken) {
+    deviceToken = uuid();
+  }
+
+  newItem.author_id = deviceToken;
 
   const filePath = path.join("data", "items.json");
   const data = fs.readFileSync(filePath, "utf8");
@@ -18,15 +42,25 @@ export async function POST(request: Request) {
 
   const existingItem = items.find((item) => item.id === newItem.id);
   if (existingItem) {
-    return NextResponse.json(
-      { message: `Item with ID ${newItem.id} already exists.` },
-      { status: 400 }
-    );
+    return NextResponse.json({
+      message: "Item with this ID already exists",
+      newItem,
+    });
   }
 
   items.push(newItem);
 
   fs.writeFileSync(filePath, JSON.stringify(items, null, 2), "utf8");
 
-  return NextResponse.json({ message: "Item added successfully", newItem });
+  const response = NextResponse.json({
+    message: "Item added successfully",
+    newItem,
+  });
+  response.cookies.set("device-token", deviceToken, {
+    httpOnly: true,
+    // secure: true, // cookies HTTP addresses with secure=true are not saved
+    maxAge: 60 * 60 * 24 * 365, // 1 year expiration
+  });
+
+  return response;
 }
