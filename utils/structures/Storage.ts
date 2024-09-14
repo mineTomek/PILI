a612926786e49599041683e332d78ae84d3972e7
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-14 19:26:35 +0200

    Make Storage’s fields optional

--- utils/structures/Storage.ts
+++ utils/structures/Storage.ts
@@ -1,11 +1,11 @@
 import DataObject from "./DataObject";
 
 type Storage = DataObject & {
-  room_id: string;
-  type: "desk" | "shelf" | "drawer" | "other";
-  group: string;
-  level: number;
-  surfacePosition: {
+  room_id?: string;
+  type?: "desk" | "shelf" | "drawer" | "other";
+  group?: string;
+  level?: number;
+  surfacePosition?: {
     x: number;
     y: number;
   };
