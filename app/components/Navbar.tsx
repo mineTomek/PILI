6de7bcabf561ea4e1b78d8dcb2951a61980e18a7
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-19 19:38:32 +0200

    Add backdrop blurring to the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,12 @@
 export default function Navbar() {
   return (
     <div className="h-12 fixed top-0 left-0 right-0">
-      <div className="flex justify-center">
-        <h1 className="font-black text-3xl my-auto tracking-wider">PILI</h1>
+      <div className="flex justify-center relative h-full">
+        <h1 className="font-black text-3xl my-auto tracking-wider px-4 z-50">
+          PILI
+        </h1>
+
+        <div className="absolute inset-0 -bottom-32 fade-out-mask backdrop-blur z-40" />
       </div>
     </div>
   );
