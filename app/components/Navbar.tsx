ab742d5c1ddab3d0dfb7d23d9c99c63ff948533e
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-19 21:03:14 +0200

    Remove pointer events from the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="h-12 fixed top-0 left-0 right-0">
+    <div className="h-12 fixed top-0 left-0 right-0 pointer-events-none">
       <div className="flex justify-center relative h-full">
         <h1 className="font-black text-3xl my-auto tracking-wider px-4 z-50">
           PILI
