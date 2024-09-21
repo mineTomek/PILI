f0af6d81c5d5eccb7932932ba1f734f9e0b8e2b7
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-21 12:30:18 +0200

    Make /api/[type]/get/[id] route more readable

--- app/api/[type]/get/[id]/route.ts
+++ app/api/[type]/get/[id]/route.ts
@@ -5,7 +5,10 @@ import DataObject from "@/utils/structures/DataObject";
 import Session from "@/utils/structures/Session";
 import { allowedTypes } from "@/utils/allowedDataTypes";
 
-export async function GET(_request: Request, { params }: { params: { type: string, id: string } }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: { type: string; id: string } }
+) {
   if (!allowedTypes.includes(params.type)) {
     return NextResponse.json({ message: "Invalid type" }, { status: 400 });
   }
@@ -15,18 +18,18 @@ export async function GET(_request: Request, { params }: { params: { type: strin
   const items: DataObject[] = JSON.parse(itemsData);
   const item = items.find((i) => i.id === params.id);
 
-  if (item) {
-    const sessionsFilePath = path.join(process.cwd(), "data", "sessions.json");
-    const sessionsData = fs.readFileSync(sessionsFilePath, "utf8");
-    const sessions: Session[] = JSON.parse(sessionsData);
-    const session = sessions.find(
-      (s) => s.id_list.indexOf(item.author_id!) != -1
-    );
-
-    item.author_session_name = session?.name ?? "Unknown ID";
-
-    return NextResponse.json({ item });
-  } else {
+  if (!item) {
     return NextResponse.json({ message: "Object not found" }, { status: 404 });
   }
+  
+  const sessionsFilePath = path.join(process.cwd(), "data", "sessions.json");
+  const sessionsData = fs.readFileSync(sessionsFilePath, "utf8");
+  const sessions: Session[] = JSON.parse(sessionsData);
+  const session = sessions.find(
+    (s) => s.id_list.indexOf(item.author_id!) != -1
+  );
+
+  item.author_session_name = session?.name ?? "Unknown ID";
+
+  return NextResponse.json({ item });
 }
