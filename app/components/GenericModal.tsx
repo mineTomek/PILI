707816a80328b63391c19582ec8ca1e3385ce408
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-09-21 12:31:10 +0200

    Display the history of an item in its modal

--- app/components/GenericModal.tsx
+++ app/components/GenericModal.tsx
@@ -7,10 +7,13 @@ import {
   faCancel,
   faChevronDown,
   faEdit,
+  faFilePen,
+  faFolderPlus,
   faSave,
   faTrashCan,
+  faTriangleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { mergeCss } from "@/utils/mergeCss";
 
@@ -33,11 +36,25 @@ export default function GenericModal<T extends DataObject>(props: {
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const [details, setDetails] = useState(false);
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const [historyVisible, setHistoryVisible] = useState(false);
+
+  const [historyEntries, setHistoryEntries] = useState<
+    HistoryEntry<DataObject>[]
+  >([]);
 
   const router = useRouter();
 
   const [item, setItem] = useState<T>(props.item);
+
+  useEffect(() => {
+    if (historyVisible && historyEntries.length === 0) {
+      fetch(`/api/${props.dataType}/history/get?item_id=${item.id}`)
+        .then((res) => res.json())
+        .then((data) => setHistoryEntries(data.entries))
+        .catch((error) => console.error("Error fetching history:", error));
+    }
+  }, [historyVisible, item.id, props.dataType]);
 
   const handleChange = (
     e: React.ChangeEvent<
@@ -179,15 +196,20 @@ export default function GenericModal<T extends DataObject>(props: {
         </>
       )}
 
-      <p className="mt-4" onClick={() => setDetails((prev) => !prev)}>
+      <p className="mt-4" onClick={() => setDetailsVisible((prev) => !prev)}>
         Details{" "}
         <FontAwesomeIcon
           icon={faChevronDown}
-          className={mergeCss("transition-transform", details && "rotate-180")}
+          className={mergeCss(
+            "transition-transform",
+            detailsVisible && "rotate-180"
+          )}
         />
       </p>
-      <div className={details ? "block" : "hidden"}>
-        <p className="text-zinc-500 dark:text-zinc-400 text-sm font-light">Item ID: {item.id}</p>
+      <div className={detailsVisible ? "block" : "hidden"}>
+        <p className="text-zinc-500 dark:text-zinc-400 text-sm font-light">
+          Item ID: {item.id}
+        </p>
         <p className="text-zinc-500 dark:text-zinc-400 text-sm font-light">
           Author ID: {item.author_id}
         </p>
@@ -198,6 +220,171 @@ export default function GenericModal<T extends DataObject>(props: {
           Creation Time:{" "}
           {item.creation_time && new Date(item.creation_time).toISOString()}
         </p>
+      </div>
+
+      <p className="mt-4" onClick={() => setHistoryVisible((prev) => !prev)}>
+        History{" "}
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className={mergeCss(
+            "transition-transform",
+            historyVisible && "rotate-180"
+          )}
+        />
+      </p>
+      <div className={historyVisible ? "block" : "hidden"}>
+        {historyEntries.length > 0 ? (
+          <div className="flex flex-col gap-2 mt-3">
+            {historyEntries
+              .toSorted((a, b) => b.timestamp - a.timestamp)
+              .map((entry) => {
+                const formatChanges = (
+                  previousState: Record<string, any>,
+                  currentState: Record<string, any>
+                ) => {
+                  const changes: JSX.Element[] = [];
+
+                  for (const key in currentState) {
+                    if (currentState[key] !== previousState[key]) {
+                      changes.push(
+                        <p>
+                          <strong>{key.replace(/_/g, " ")}</strong> changed from{" "}
+                          <i>"{previousState[key]}"</i> to{" "}
+                          <i>"{currentState[key]}"</i>
+                        </p>
+                      );
+                    }
+                  }
+
+                  return changes;
+                };
+
+                const findPreviousEntry = () => {
+                  const currentEntryTimestamp = entry.timestamp;
+
+                  const previousEntries = historyEntries.filter(
+                    (e) => e.timestamp < currentEntryTimestamp
+                  );
+
+                  const sortedEntries = previousEntries.sort(
+                    (a, b) => b.timestamp - a.timestamp
+                  );
+
+                  const previousEntry =
+                    sortedEntries.length > 0 ? sortedEntries[0] : null;
+
+                  return previousEntry;
+                };
+
+                const customTags = entry.tags.filter(
+                  (tag) =>
+                    tag !== "created" && tag !== "deleted" && tag !== "edited"
+                );
+
+                let content: JSX.Element | JSX.Element[] = Object.entries(
+                  entry.state
+                ).map(([key, value]) => (
+                  <div key={key}>
+                    <p>
+                      <strong>{key.replace(/_/g, " ")}: </strong>{" "}
+                      {String(value)}
+                    </p>
+                  </div>
+                ));
+
+                if (entry.tags.includes("edited")) {
+                  const previousEntry = findPreviousEntry();
+
+                  content = previousEntry ? (
+                    formatChanges(previousEntry.state, entry.state).map(
+                      (change, index) => <div key={index}>{change}</div>
+                    )
+                  ) : (
+                    <div>No previous changes found for this entry</div>
+                  );
+                }
+
+                return (
+                  <div
+                    key={entry.id}
+                    className="bg-zinc-200 dark:bg-zinc-700 p-2 rounded-lg relative"
+                  >
+                    <div className="flex flex-col absolute top-3 left-3 gap-3">
+                      {entry.tags.map((tag) => {
+                        let className = "text-yellow-500 bg-yellow-800";
+                        let icon = faTriangleExclamation;
+
+                        if (tag === "created") {
+                          className = "text-green-500 bg-green-800";
+                          icon = faFolderPlus;
+                        }
+
+                        if (tag === "edited") {
+                          className = "text-sky-500 bg-sky-800";
+                          icon = faFilePen;
+                        }
+
+                        if (tag === "deleted") {
+                          className = "text-red-500 bg-red-800";
+                          icon = faTrashCan;
+                        }
+
+                        return (
+                          <FontAwesomeIcon
+                            icon={icon}
+                            className={mergeCss(
+                              "w-[1em] p-2 rounded-md shadow-lg",
+                              className
+                            )}
+                          />
+                        );
+                      })}
+                    </div>
+
+                    <p
+                      className={mergeCss(
+                        "text-sm text-zinc-700 dark:text-zinc-200 ml-12"
+                      )}
+                    >
+                      <strong>Timestamp:</strong>{" "}
+                      {new Date(entry.timestamp).toLocaleString()}
+                    </p>
+                    <p
+                      className={mergeCss(
+                        "text-sm text-zinc-700 dark:text-zinc-200 ml-12"
+                      )}
+                    >
+                      <strong>Author:</strong> {entry.author_session_name} (
+                      {entry.author_id})
+                    </p>
+                    {customTags.length > 0 && (
+                      <p
+                        className={mergeCss(
+                          "text-sm text-zinc-700 dark:text-zinc-200 ml-12"
+                        )}
+                      >
+                        <strong>Tags:</strong> {customTags.join(", ")}
+                      </p>
+                    )}
+                    <div
+                      className={mergeCss(
+                        "text-sm text-zinc-700 dark:text-zinc-200 ml-12"
+                      )}
+                    >
+                      <strong>State:</strong>{" "}
+                      <div className="ml-1 pl-2 border-l-2 border-zinc-400">
+                        {content}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        ) : (
+          <p className="text-sm text-zinc-500 dark:text-zinc-400">
+            No history entries available.
+          </p>
+        )}
       </div>
 
       <div className="absolute top-2 right-2 flex gap-2">
